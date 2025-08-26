4424cc0ebf12eee919299c0df77f655ce37cdc80
MiguelHI95 <99383884+MiguelHI95@users.noreply.github.com>
2025-08-26 16:35:49 -0600

    app6.js

---
+++ app6.js
@@ -0,0 +1,13 @@
+let variable1 = parseFloat (prompt("ingresa primer valor"));
+let variable2 = parseFloat (prompt("ingresa segundo valor"));
+
+
+let suma = variable1 + variable2;
+let resta = variable1 - variable2;
+let multiplicacion =  variable1 * variable2;
+let division =  variable1 / variable2;
+
+console.log("Resultado de suma es: " + suma);
+console.log("Resultado de resta es: " + resta);
+console.log("Resultado de multiplicacion es: " + multiplicacion);
+console.log("Resultado de division es: " divison);
